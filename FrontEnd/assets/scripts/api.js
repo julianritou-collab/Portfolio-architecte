767b53e87214f241ab2e1a4d159ac48823f3cc72
Julian Ritou <julian.ritou@gmail.com>
2026-02-23 15:54:19 +0100

    sauvegarde des works dans le sessionStorage + suppression des figures locales d'exemple

--- FrontEnd/assets/scripts/api.js
+++ FrontEnd/assets/scripts/api.js
@@ -3,12 +3,21 @@ console.log("api.js chargé");
 
 export const getWorks = async () => {
     try {
-        const response = await fetch('http://localhost:5678/api/works');
-        if (!response.ok) {
-            throw new Error('Erreur lors de la récupération des travaux');
+        const works = window.sessionStorage.getItem("works");
+        if (works) {
+            console.log("Travaux récupérés depuis le sessionStorage");
+            return JSON.parse(works);
         }
-        const data = await response.json();
-        return data;
+        else{
+            const response = await fetch('http://localhost:5678/api/works');
+            if (!response.ok) {
+                throw new Error('Erreur lors de la récupération des travaux');
+            }
+            const data = await response.json();
+            window.sessionStorage.setItem("works", JSON.stringify(data));
+            console.log("Travaux récupérés depuis l'API et stockés dans le sessionStorage");
+            return data;
+        }
     } catch (error) {
         console.error(error);
         return [];
