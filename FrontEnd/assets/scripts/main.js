82f74c972ab6f89a5116f3abe7e827d8df98c96c
Julian Ritou <julian.ritou@gmail.com>
2026-02-25 19:08:58 +0100

    sauvegarde mode edition à finir

--- FrontEnd/assets/scripts/main.js
+++ FrontEnd/assets/scripts/main.js
@@ -4,16 +4,35 @@ import { displayWorks, displayCategories , filterManager } from './works.js'
 
 console.log("main.js chargé");
 
+const logoutManager = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    window.location.href = "index.html";
+}
+
 const main = async () => {
     console.log("main() appelé");
+    // Vérifier si l'utilisateur est connecté
+    let modeEdit = localStorage.getItem("token") ? true : false;
+    // Afficher le mode édition si l'utilisateur est connecté
+    if(modeEdit) {
+        document.querySelector(".edit-ribbon").style.display = "flex";
+        document.querySelector(".login-link").style.display = "none";
+        document.querySelector(".logout-link").style.display = "block";
+        document.querySelector(".cta-logout").addEventListener("click", (e) => {
+            e.preventDefault();
+            logoutManager();
+        });
+    }
     // Récupérer les travaux depuis l'API
     const works = await getWorks();
     console.log(works);
     // Récupérer les catégories depuis l'API
     const categories = await getCategories();
     console.log(categories);
-    // Afficher les catégories
-    displayCategories(categories);
+    // Afficher les catégories si l'utilisateur n'est pas connecté
+    if(!modeEdit) 
+        displayCategories(categories);
     // Afficher les travaux
     displayWorks(works);
     // filtrer les travaux par catégorie
